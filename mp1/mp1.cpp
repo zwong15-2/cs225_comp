a380fc607b59fddc3d0261edbe24506f1075250b
Zach Wong <zrwong2@linux-17.ews.illinois.edu>
2018-09-06 15:51:22 -0500

    mp1 complete

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -9,12 +9,42 @@ using cs225::HSLAPixel;
 
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+   PNG png_rotate;
+   png_rotate.readFromFile(inputFile);
+   PNG copy_png = PNG(png_rotate); 
+   for(unsigned x =  0; x < png_rotate.width(); x++){
+     for(unsigned y = 0; y < png_rotate.height(); y++){
+       png_rotate.getPixel(png_rotate.width()-x-1, png_rotate.height()-y-1) = copy_png.getPixel(x,y);
+     }
+   }
+   png_rotate.writeToFile(outputFile);
 }
 
 PNG myArt(unsigned int width, unsigned int height) {
   PNG png(width, height);
-  // TODO: Part 3
-
+  for(unsigned x = 0; x < png.width()/3; x++){
+    for(unsigned y = 0; y < png.height()/3; y++){
+      HSLAPixel & pixel = png.getPixel(x,y);
+      pixel.l = 0.6;
+      pixel.a = 0.2;
+      pixel.h = 1;
+    }
+  }
+  for(unsigned a = 267; a < 533; a++){
+    for(unsigned b = 267; b < 533; b++){
+      HSLAPixel & pixel_2 = png.getPixel(a,b);
+      pixel_2.l = 0.2;
+      pixel_2.s = 10;
+      pixel_2.h = 223;
+    }
+  }
+  for(unsigned p = 534; p < png.width(); p++){
+    for(unsigned q = 534; q < png.height(); q++){
+      HSLAPixel & pixel_3 = png.getPixel(p,q);
+      pixel_3.a = 0.6;
+      pixel_3.l = 0.23;
+      pixel_3.h = 300;
+    }
+  }
   return png;
 }

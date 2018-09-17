d72a9f656a4aa18bd621a85bc24047375c407303
Zach Wong <zrwong2@linux-24.ews.illinois.edu>
2018-09-16 23:19:32 -0500

    lab

--- lab_memory/letter.cpp
+++ lab_memory/letter.cpp
@@ -3,9 +3,12 @@
  */
 
 #include "letter.h"
+#include <iostream>
+using namespace std;
 
-Letter::Letter() : letter('-'), count(0)
-{
+Letter::Letter(){
+    letter = '-';
+    count = 0;
 }
 
 void Letter::addStudent()
@@ -13,7 +16,27 @@ void Letter::addStudent()
     count++;
 }
 
+Letter::Letter(const Letter& other)
+{
+    copy(other);
+}
+
+
 bool Letter::operator<(const Letter& other) const
 {
     return count > other.count;
+}
+
+Letter& Letter::operator=(const Letter& other)
+{
+    if (this != &other) {
+	     copy(other);
+    }
+    return *this;
+}
+
+void Letter::copy(const Letter& other)
+{
+    count = other.count;
+    letter = other.letter;
 }

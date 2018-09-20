494b6c667808eb87f51bc65a59d57d76a020fe5d
Zach Wong <zrwong2@linux-16.ews.illinois.edu>
2018-09-19 22:42:53 -0500

    test

--- mp2/Image.h
+++ mp2/Image.h
@@ -5,3 +5,42 @@
 
 #pragma once
 
+#include "cs225/PNG.h"
+using cs225::PNG;
+#include "cs225/HSLAPixel.h"
+using cs225::HSLAPixel;
+
+#include <string>
+using std::string;
+
+class Image : public PNG{
+
+public:
+       
+	void lighten();
+
+	void lighten(double amount);
+
+	void darken();
+
+	void darken(double amount);
+
+	void saturate();
+
+	void saturate(double amount);
+
+	void desaturate();
+
+	void desaturate(double amount);
+
+	void grayscale();
+
+	void rotateColor(double degrees);
+
+	void illinify();
+
+	void scale(double factor);
+
+	void scale(unsigned w, unsigned h);
+
+};

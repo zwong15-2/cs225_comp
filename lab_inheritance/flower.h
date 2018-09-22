26b415e82870e91501debc424c1ddc55a9a4c3f5
Zach Wong <zrwong2@linux-18.ews.illinois.edu>
2018-09-22 17:05:23 -0500

    lab

--- lab_inheritance/flower.h
+++ lab_inheritance/flower.h
@@ -9,17 +9,21 @@
 #pragma once
 
 #include "shape.h"
+#include "circle.h"
+#include "rectangle.h"
+#include "triangle.h"
 
 class Flower : public Drawable
 {
-  private:
-    Shape stem;
-    Shape pistil; // center piece of flower
-    Shape leaf;
+  private: 
+    Shape * stem;
+    Shape * pistil; // center piece of flower
+    Shape * leaf;
 
     void drawPetals(PNG* canvas, const Vector2& center, int x, int y) const;
 
   public:
     Flower(const Vector2& center);
     void draw(PNG* canvas) const;
+    ~Flower();
 };

2beddca19d0325df5f8838afce7ad9c2b6f989d5
Zach Wong <zrwong2@linux-08.ews.illinois.edu>
2018-09-22 23:11:05 -0500

    mp

--- mp2/main.cpp
+++ mp2/main.cpp
@@ -1,5 +1,9 @@
 #include "Image.h"
 #include "StickerSheet.h"
+#include "./cs225/PNG.h"
+#include "./cs225/HSLAPixel.h"
+
+using namespace cs225;
 
 int main() {
 
@@ -7,6 +11,23 @@ int main() {
   // Reminder:
   //   Before exiting main, save your creation to disk as myImage.png
   //
+
+ Image alma; alma.readFromFile("alma.png");
+ Image i;    i.readFromFile("i.png");
+ Image lightning; lightning.readFromFile("mcqueen.png");
+ Image barney; barney.readFromFile("barney.png");
+ Image youtube; youtube.readFromFile("youtube.png");
+
+ StickerSheet sheet(alma, 4);
+ sheet.addSticker(i, 15, 200);
+ sheet.addSticker(lightning, 1, 1);
+ sheet.addSticker(barney, 2, 2);
+ sheet.addSticker(youtube, 6, 5);
+ Image result = sheet.render();
+ result.writeToFile("MyImage.png");
+
+
+
 
   return 0;
 }

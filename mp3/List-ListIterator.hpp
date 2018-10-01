a9a0e167fae294fee7ef735f31923954be844d8a
Zach Wong <zrwong2@linux-a1.ews.illinois.edu>
2018-10-01 16:14:49 -0500

    mp

--- mp3/List-ListIterator.hpp
+++ mp3/List-ListIterator.hpp
@@ -10,6 +10,7 @@ class ListIterator : public std::iterator<std::bidirectional_iterator_tag, T> {
     // Pre-Increment, ++iter
     ListIterator& operator++() {
         // @TODO: graded in MP3.1
+        position_ = position_->next; 
         return *this;
     }
     
@@ -29,7 +30,9 @@ class ListIterator : public std::iterator<std::bidirectional_iterator_tag, T> {
     // Post-Decrement, iter--
     ListIterator operator--(int) {
         // @TODO: graded in MP3.1
-        return ListIterator();
+        ListNode *temp = position_;
+		position_ = position_->prev;
+        return ListIterator(temp);
     }
 
     bool operator==(const ListIterator& rhs) {
@@ -38,6 +41,9 @@ class ListIterator : public std::iterator<std::bidirectional_iterator_tag, T> {
 
     bool operator!=(const ListIterator& rhs) {
         // @TODO: graded in MP3.1
+        if(this->position_ != rhs.position_){
+			return true;
+		}
         return false;
     }
 

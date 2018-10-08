2b134635a878916d8e3e3c3fb66d36cdebb4536b
Zach Wong <zrwong2@linux-02.ews.illinois.edu>
2018-10-07 23:55:41 -0500

    lab

--- lab_trees/abstractsyntaxtree.cpp
+++ lab_trees/abstractsyntaxtree.cpp
@@ -1,4 +1,6 @@
 #include "abstractsyntaxtree.h"
+#include <string>
+using namespace std;
 
 /**
  * Calculates the value from an AST (Abstract Syntax Tree). To parse numbers from strings, please use std::stod
@@ -6,6 +8,36 @@
  */
 double AbstractSyntaxTree::eval() const {
     // @TODO Your code goes here...
-    return -1;
+    return evaluation(root);
 }
 
+double AbstractSyntaxTree::evaluation(Node *root) const{
+
+
+if(!root){
+	return 0;
+}
+
+if(!root->left &&  !root->right){
+	string element = root->elem;
+	double number = stod(element);
+	return number;
+}
+
+double left_value = evaluation(root->left);
+double right_value = evaluation(root->right);
+
+if(root->elem == "+"){
+	return (left_value + right_value);
+}
+
+if(root->elem == "-"){
+	return (left_value - right_value);
+}
+
+if(root->elem == "*"){
+	return (left_value * right_value);
+}
+
+return (left_value/right_value);
+}

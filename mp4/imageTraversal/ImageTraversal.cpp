ff2fbcbb0ec6422fc8a9042ff6132ded5712a9d0
Zach Wong <zrwong2@linux-12.ews.illinois.edu>
2018-10-22 23:51:10 -0500

    mp

--- mp4/imageTraversal/ImageTraversal.cpp
+++ mp4/imageTraversal/ImageTraversal.cpp
@@ -8,6 +8,8 @@
 
 #include "ImageTraversal.h"
 
+using namespace cs225;
+
 /**
  * Calculates a metric for the difference between two pixels, used to
  * calculate if a pixel is within a tolerance.
@@ -31,17 +33,92 @@ double ImageTraversal::calculateDelta(const HSLAPixel & p1, const HSLAPixel & p2
 /**
  * Default iterator constructor.
  */
-ImageTraversal::Iterator::Iterator() {
+ImageTraversal::Iterator::Iterator() : traversal(NULL) {
   /** @todo [Part 1] */
+  
+}
+
+ImageTraversal::Iterator::Iterator(ImageTraversal &traversal, Point starting) : traversal(&traversal), starting(starting)
+{
+	current = starting;
+}
+
+ImageTraversal::Iterator::Iterator(const ImageTraversal::Iterator &other){
+	traversal = other.traversal;	
 }
 
 /**
- * Iterator increment opreator.
+i * Iterator increment opreator.
  *
  * Advances the traversal of the image.
  */
 ImageTraversal::Iterator & ImageTraversal::Iterator::operator++() {
   /** @todo [Part 1] */
+
+	// current = traversal->peek();
+	//current = traversal->pop();
+
+	if(traversal->check_visited(current.x, current.y) != true){
+		
+		traversal->mark_visited(current);
+		PNG temp = traversal->get_png();
+		HSLAPixel & pixel2 = temp.getPixel(starting.x, starting.y);			
+	
+		if(current.x+1 < (unsigned)temp.width() && traversal->check_visited(current.x+1,current.y) != true /*&& !traversal->check_added(current.x+1, current.y)*/){
+
+			HSLAPixel & pixel1 = temp.getPixel(current.x+1, current.y);
+
+			if(calculateDelta(pixel1, pixel2) < traversal->get_tolerance() ){
+				Point *point_right = new Point(current.x + 1, current.y);
+				traversal->add(*point_right);
+				traversal->mark_added(current.x+1, current.y);
+				}
+			}
+
+		if( current.y + 1 < (unsigned)temp.height() && traversal->check_visited(current.x,current.y+1) != true /*&& !traversal->check_added(current.x, current.y+1)*/){
+
+			HSLAPixel & pixel1 = temp.getPixel(current.x, current.y+1);
+
+			if(calculateDelta(pixel1, pixel2) < traversal->get_tolerance() ){
+				Point *point_below = new Point(current.x, current.y + 1);
+				traversal->add(*point_below);
+				traversal->mark_added(current.x, current.y+1);
+				}
+			}	
+
+		if((int)current.x - 1 >= 0 && traversal->check_visited(current.x-1,current.y) != true /*&& !traversal->check_added(current.x-1, current.y)*/){
+
+			HSLAPixel & pixel1 = temp.getPixel(current.x-1, current.y);
+
+			if(calculateDelta(pixel1, pixel2) < traversal->get_tolerance() ){
+				Point *point_left = new Point(current.x - 1, current.y);
+				traversal->add(*point_left);
+				traversal->mark_added(current.x-1, current.y);
+				}
+			}
+		if((int)current.y-1 >= 0 && traversal->check_visited(current.x,current.y-1) != true /*&& !traversal->check_added(current.x, current.y-1)*/){
+
+			HSLAPixel & pixel1 = temp.getPixel(current.x, current.y-1);
+
+			if(calculateDelta(pixel1, pixel2) < traversal->get_tolerance() ){
+				Point *point_above = new Point(current.x, current.y - 1);
+				traversal->add(*point_above);
+				traversal->mark_added(current.x, current.y-1);
+				}
+			}
+		}
+ 	if (!traversal->empty()) {
+		current = traversal->peek();
+		while(traversal->check_visited(current.x, current.y) == true){
+			if(traversal->empty() != true){	
+				current = traversal->pop();	
+			}
+			else{
+				return *this;
+			}
+		}
+	}
+	current = traversal->pop();
   return *this;
 }
 
@@ -52,7 +129,7 @@ ImageTraversal::Iterator & ImageTraversal::Iterator::operator++() {
  */
 Point ImageTraversal::Iterator::operator*() {
   /** @todo [Part 1] */
-  return Point(0, 0);
+  return current;
 }
 
 /**
@@ -62,6 +139,23 @@ Point ImageTraversal::Iterator::operator*() {
  */
 bool ImageTraversal::Iterator::operator!=(const ImageTraversal::Iterator &other) {
   /** @todo [Part 1] */
-  return false;
+  bool this_empty = false;
+  bool other_empty = false;
+
+  if(traversal == NULL) {this_empty = true; }
+  if(other.traversal == NULL) {other_empty = true; }
+
+  if(!this_empty) {this_empty = traversal->empty(); }
+  if(!other_empty) {other_empty = other.traversal->empty(); }
+
+  if(this_empty && other_empty){
+	 return false;
+  }
+  else if((!this_empty) && (!other_empty)){
+ 	 return(traversal != other.traversal);
+  }
+  else{
+	return true;
+  }
 }
 

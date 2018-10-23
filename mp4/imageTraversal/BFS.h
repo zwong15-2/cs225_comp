f2507a9edf1b98f602457d0d4130278abaaba8ab
Zach Wong <zrwong2@linux-12.ews.illinois.edu>
2018-10-22 23:50:59 -0500

    mp

--- mp4/imageTraversal/BFS.h
+++ mp4/imageTraversal/BFS.h
@@ -31,8 +31,23 @@ public:
   Point pop();
   Point peek() const;
   bool empty() const;
+  double get_tolerance();
+  PNG get_png();
+  void mark_visited(const Point & point);
+  bool check_visited(unsigned x, unsigned y);
+  void mark_added(unsigned x, unsigned y);
+  bool check_added(unsigned x, unsigned y);
 
 private:
-  /** @todo [Part 1] */
-  /** add private members here*/
+	/** @todo [Part 1] */
+	/** add private members here*/
+	std::queue<Point> png_visit;
+	std::vector<std::vector<bool> > visited;
+	std::vector<std::vector<bool> > added;
+	int png_height;
+	int png_width;
+	bool val;
+	double check_tolerance;
+	Point starting_point;
+	PNG image;
 };

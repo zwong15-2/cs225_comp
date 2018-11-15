3c9d969f11c930ac0ecca8e187dca2da52626697
Zach Wong <zrwong2@linux-05.ews.illinois.edu>
2018-11-15 17:20:25 -0600

    mp

--- mp6/dsets.h
+++ mp6/dsets.h
@@ -1,1 +1,19 @@
 /* Your code here! */
+#pragma once
+#include <vector>
+
+using std::vector;
+
+class DisjointSets
+{
+
+	public:
+		void addelements(int num);
+		int find(int elem);
+		void setunion(int a, int b);
+		int size(int elem);
+
+	private:
+		vector<int> sets;
+
+};

713fb1ac721199ba0ca5771d96f1dbb847067b86
Zach Wong <zrwong2@linux-a1.ews.illinois.edu>
2018-11-27 20:33:56 -0600

    mp

--- mp6/maze.h
+++ mp6/maze.h
@@ -11,23 +11,34 @@
 using namespace std;
 using namespace cs225;
 
-
+//Class contains the SquareMaze object. This class
+//constructs a randomly generated square maze.
 class SquareMaze{
 
 	public:
+		//Default Constructor
 		SquareMaze();
-
+		
+		//Makes a new SquareMaze object with a given width and height
 		void makeMaze(int width, int height);
 
+		//Determines whether it is possible to travel in direction of x,y coordinates
 		bool canTravel(int x, int y, int dir) const;
-
+		
+		//Sets whether not a specified wall exists
 		void setWall(int x, int y, int dir, bool exists);
 
+		//Solves the SquareMaze
 		vector<int> solveMaze();
 
+		//Draws the maze but does not contain the solution
 		cs225::PNG * drawMaze() const;
 
+		//Draws the maze with and its solution
 		cs225::PNG * drawMazeWithSolution();
+
+		//Draws creative maze for Part 3
+		PNG * drawCreativeMaze();
 
 	private:
 		int width;

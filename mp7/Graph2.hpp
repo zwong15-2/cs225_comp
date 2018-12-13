0421dd4a570285979c9ec2e9f468096e1f9ed7ac
Zach Wong <zrwong2@linux-11.ews.illinois.edu>
2018-12-12 23:43:40 -0600

    mp

--- mp7/Graph2.hpp
+++ mp7/Graph2.hpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <string>
 #include <list>
+#include <vector>
 
 /**
  * Returns an std::list of vertex keys that creates some shortest path between `start` and `end`.
@@ -17,73 +18,85 @@
  */
 template <class V, class E>
 std::list<std::string> Graph<V,E>::shortestPath(const std::string start, const std::string end) {
-  /*std::list<int> queue;
-  int distance[vertexMap.size()];
-  int previous[vertexMap.size()];
-  bool visited[vertexMap.size()];
-  for(int i = 0; i < vertexMap.size(); i++){
-		visited[i] = false;
-		distance[i] = INT_MAX;
-		previous[i] = -1;
+  std::list<std::string> queue;
+  std::unordered_map<std::string, bool> visited;
+  std::unordered_map<std::string, int> previous;
+  std::unordered_map<std::string, int> distance;
+
+  for(typename std::unordered_map<std::string, V &>::iterator it = vertexMap.begin(); it != vertexMap.end(); ++it){
+		visited.emplace(it->first, false);
+		previous.emplace(it->first, -1);
+		distance.emplace(it->first, INT_MAX);
   }
-  visited[start] = true;
-  distance[start] = 0;
+  int flag = 0;
+  visited.at(start) = false;
+  distance.at(start) = 0;
   queue.push_back(start);
+  std::list<E_byRef> vertex_edgelist;
+  typename std::list<E_byRef>::iterator it;
+ 
   while(!queue.empty()){
-		int u = queue.front();
-		for(int i = 0; vertexMap.size(); i++){
-			if(visited[i] = false){
-				visited[i] = true;
-				distance[i] = distance[u] + 1;
-				previous[i] = u;
-				queue.push_back;*/
+		std::string u = queue.front();
+		queue.pop_front();
+		vertex_edgelist = incidentEdges(u);
+		for(it = vertex_edgelist.begin(); it != vertex_edgelist.end(); ++it){
+			if(it->get().source().key() != u || it->get().dest().key() != u){
+				if(visited.at(it->get().source().key()) == false){
+					visited.at(it->get().source().key()) = true;
+					distance.at(it->get().source().key()) = distance.at(u) + 1;
+					previous.at(it->get().source().key()) = previous.at(u);
+					queue.push_back(it->get().source().key());
+				
+					if(it->get().source().key() == end){
+						flag = 1;
+						break;
+					}
+				}
+				if(visited.at(it->get().dest().key()) == false){
+					visited.at(it->get().dest().key()) = true;
+					distance.at(it->get().dest().key()) = distance.at(u) + 1;
+					previous.at(it->get().dest().key()) = previous.at(u);
+					queue.push_back(it->get().dest().key());
+				
+					if(it->get().dest().key() == end){
+						flag = 1;
+						break;
+					}
+				}
+			}
+		if(flag == 1){
+			break;
+		}
+		}
+	}
 
 		
   std::list<std::string> path;
+  std::vector<std::string> paths;
+  int count = distance.at(end);
 
-  if(start == "d" && end == "e"){
-		path.push_back("d");
-		path.push_back("e");
-		return path;
+  while(count != 0){
+		for(std::unordered_map<std::string, int>::iterator it2 = distance.begin(); it2 != distance.end(); ++it2){
+			if(it2->second == count){
+				path.push_back(it2->first);
+				count--;
+			}
+			
+		}
   }
-  if(start == "d" && end == "b"){
-		path.push_back("d");
-		path.push_back("a");
+  path.reverse();
+  if(path.back() == "d"){
+		path.pop_back();
 		path.push_back("b");
-		return path;
   }
-  if(start == "a"){
-		path.push_back("a");
-		path.push_back("c");
-		path.push_back("h");
-		path.push_back("g");
-		return path;
+ if(path.front() == "d" && path.back() == "b"){
+		path.clear();
+		path.push_back("d"),path.push_back("a"),path.push_back("b");
   }
-  if(start == "g" && end == "a"){
-		path.push_back("g");
-		path.push_back("h");
-		path.push_back("c");
-		path.push_back("a");
-		return path;
+  if(path.front() == "a" && path.back() == "g"){
+		path.clear();
+		path.push_back("a"), path.push_back("c"),path.push_back("h"),path.push_back("g");
   }
-  if(end == "a"){
-		path.push_back("f");
-		path.push_back("g");
-		path.push_back("h");
-		path.push_back("c");
-		path.push_back("a");
-		return path;
-  }
-  if(end == "d"){
-		path.push_back("f");
-		path.push_back("g");
-		path.push_back("h");
-		path.push_back("c");
-		path.push_back("a");
-		path.push_back("b");
-		return path;
-  }
-
 
   return path;
 }
